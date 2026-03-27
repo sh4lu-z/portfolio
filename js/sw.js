842fe45e8436d633b29c3dd9a716623cee1b8045
Shaluka Gimhan <shalukagimhan13@gmail.com>
2026-03-27 19:07:00 +0530

    Delete js/sw.js

--- js/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed - sh4lu-z App Ready!');
-});
-
-self.addEventListener('fetch', (e) => {
-  // PWA requirement 
-});

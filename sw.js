4233e120445b15057e0527359ae9a80ce0410854
Shaluka Gimhan <shalukagimhan13@gmail.com>
2026-03-27 19:07:32 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed - sh4lu-z App Ready!');
+});
+
+self.addEventListener('fetch', (e) => {
+  // PWA requirement එක සම්පූර්ණ කරන්න විතරක් දැනට මේක ඇති
+});
